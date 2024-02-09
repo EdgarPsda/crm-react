c3e2cb1abc81e0f267d44daeef1b86297f5be724
EdgarPsda <edgar.psda@gmail.com>
2024-02-08 20:49:42 -0800

    Running JSON server
    WIP: Running JSON server.

--- src/pages/NewClient.jsx
+++ src/pages/NewClient.jsx
@@ -1,12 +1,35 @@
-import { Form, useNavigate } from "react-router-dom"
+import { Form, useActionData, useNavigate } from "react-router-dom"
 import ClientForm from "../components/ClientForm"
+import Error from "../components/Error";
 
-export function action() {
-    console.log('Submit formulario');
+export async function action({ request }) {
+    const formData = await request.formData();
+
+    const data = Object.fromEntries(formData);
+
+    const email = formData.get('email');
+
+    // Validate
+    const errors = [];
+    if (Object.values(data).includes('')) {
+        errors.push('All fields are required');
+    }
+
+    let regex = new RegExp("([!#-'*+/-9=?A-Z^-~-]+(\.[!#-'*+/-9=?A-Z^-~-]+)*|\"\(\[\]!#-[^-~ \t]|(\\[\t -~]))+\")@([!#-'*+/-9=?A-Z^-~-]+(\.[!#-'*+/-9=?A-Z^-~-]+)*|\[[\t -Z^-~]*])");
+
+    if (!regex.test) {
+        errors.push('Email not valid');
+    }
+
+    if (Object.keys(errors).length) {
+        return errors;
+    }
+
 }
 
 function NewClient() {
 
+    const errors = useActionData();
     const navigate = useNavigate();
 
     return (
@@ -24,6 +47,7 @@ function NewClient() {
             </div>
 
             <div className="bg-white shadow rounded-md md:w-3/4 mx-auto px-5 py-10 mt-20">
+                {errors?.length && errors.map((error, i) => <Error key={i}>{error}</Error>)}
                 <Form
                     method="POST"
                 >
